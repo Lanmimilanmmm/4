df71e5cb438732aa58c693e013f1d321cd24f39b
Lanmimilanmmm <90776184+Lanmimilanmmm@users.noreply.github.com>
2021-09-15 15:43:56 +0200

    Add files via upload

---
+++ this/pay/cancel.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ <div class="container">
+    <div class="status">
+        <h1 class="error">Your PayPal Transaction has been Canceled</h1>
+    </div>
+    <a href="index.php" class="btn-link">Back to Products</a>
+</div>
+</body>
+</html>
